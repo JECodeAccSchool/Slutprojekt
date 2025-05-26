a1bde177d957d777274f256664985b8573fa12a4
JECodeAccSchool <12JE0527@amal.se>
2025-05-26 12:59:07 +0200

    Slutprojekt 05-26 (2)
    Fiender, kan ej skada spelaren ,men kan dödas. Använder väldigt simpel AI, tänker göra olika för olika fiender.

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 import random
 import math
+
+import Enemy
+
 import Detail_back
 import pygame
 
@@ -23,10 +26,20 @@ def load_room(rx, ry): #ritar alla kvadrater enligt rum-arrayerna
                 Detail_front.Detail_front((x, y))
             if col == "B":
                 Detail_back.Detail_back((x, y))
+            if col == "n":
+                load_enemy("normal", x, y)
+            if col == "u":
+                load_enemy("ultra", x, y)
+            if col == "f":
+                load_enemy("flying", x, y)
+            if col == "r":
+                load_enemy("ranged", x, y)
             x += 16
         y += 16
         x = 0
 
+def load_enemy(type, pos_x, pos_y):
+    Enemy.Enemy(type, pos_x, pos_y, len(Enemy.enemies))
 
 pygame.init()
 
@@ -36,7 +49,7 @@ screen = pygame.display.set_mode((1280, 660))
 clock = pygame.time.Clock()
 running = True
 dt = 0
-rx = 2
+rx = 5
 ry = 4
 grounded = False
 grounded1 = False
@@ -59,6 +72,50 @@ weapon_mode = "normal"
 
 
 #alla rum
+roomTEST = [
+"WWWWW                                                                      WWWWW",
+"WWWWW                                                                      WWWWW",
+"WWWWW                                                                      WWWWW",
+"WWWWW                                                                      WWWWW",
+"WWWWW                                                                      WWWWW",
+"WWWWW                                                                      WWWWW",
+"WWWWW                                                                      WWWWW",
+"WWWWW                                                                      WWWWW",
+"WWWWW                                                                      WWWWW",
+"WWWWW                                                                      WWWWW",
+"WWWWW                                                                      WWWWW",
+"WWWWW                                                                      WWWWW",
+"WWWWWWWWWW                 WWWWW   WWWWW   WWWWW   WWWWW   WWWWW           WWWWW",
+"WWWWWWWWWW                 W   W   W   W   W   W   W   W   W   W           WWWWW",
+"WWWWWWWWWW                 W n W   W n W   W n W   W n W   W n W           WWWWW",
+"WWWWWWWWWW                 W   W   W   W   W   W   W   W   W   W           WWWWW",
+"WWWWWWWWWW                 WWWWW   WWWWW   WWWWW   WWWWW   WWWWW           WWWWW",
+"WWWWW                                                                      WWWWW",
+"WWWWW                                                                      WWWWW",
+"WWWWWWWWWWWWWWW                                                            WWWWW",
+"WWWWWWWWWWWWWWW                                                            WWWWW",
+"WWWWWWWWWWWWWWW                                                            WWWWW",
+"WWWWWWWWWWWWWWW                                                            WWWWW",
+"WWWWWWWWWWWWWWW                                                            WWWWW",
+"WWWWWWWWWWWWWWW                                                            WWWWW",
+"WWWWW                                                                      WWWWW",
+"WWWWW                                                                      WWWWW",
+"WWWWWWWWWWWWWWWWWWWWW                                                      WWWWW",
+"WWWWWWWWWWWWWWWWWWWWW                                                      WWWWW",
+"WWWWWWWWWWWWWWWWWWWWW                                                      WWWWW",
+"WWWWW      WWWWWWWWWW                                                      WWWWW",
+"WWWWW      WWWWWWWWWW                                                      WWWWW",
+"WWWWW      WWWWWWWWWW                                                      WWWWW",
+"WWWWW      WWWWWWWWWW                                                      WWWWW",
+"WWWWW      WWWWWWWWWW                                                      WWWWW",
+"WWWWW                                    WWWW                              WWWWW",
+"WWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWW",
+"WWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWW",
+"WWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWW",
+"WWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWW",
+"WWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWW"
+]
+
 room40 = [
 "                                                                                ",
 "                                                                                ",
@@ -156,7 +213,7 @@ room42 = [
 "      VVVVVVV                                                                   ",
 "      FFFFFFVB                                                                  ",
 "      FFFFFFVB                                                                  ",
-"      FFFFFFVB                                                                  ",
+"      FFFFFFVB                   n                                              ",
 "      FFFFFFVB                                                                  ",
 "      FFFFFFVB                                                                  ",
 "      FFFFFFVB                                                                  ",
@@ -285,40 +342,48 @@ rooms = ["room00", "room01", "room02", "room03", "room04",
          "room10", "room11", "room12", "room13", "room14",
          "room20", "room21", "room22", "room23", "room24",
          "room30", "room31", "room32", "room33", "room34",
-         room40, room41, room42, room43, room44]
+         "room40", "room41", "room42", "room43", "room44", roomTEST]
 
+load_room(rx, ry)
 
 while running:
     #töm skärmen
     screen.fill("black")
 
-    #byt rum vid kanter
+    #byt rum vid kanter och ladda rum samt ta bort saker
     if player.rect.x < 10:
         player.rect.x = 1260
         rx -= 1
         Particle.particles = []
         Particle.part_trail = []
+        Enemy.enemies = []
+        load_room(rx, ry)
     if player.rect.x > 1270:
         player.rect.x = 20
         rx += 1
         Particle.particles = []
         Particle.part_trail = []
+        Enemy.enemies = []
+        load_room(rx, ry)
     if player.rect.y < 10:
         player.rect.y = 640
         ry -= 1
         Particle.particles = []
         Particle.part_trail = []
+        Enemy.enemies = []
+        load_room(rx, ry)
     if player.rect.y > 650:
         player.rect.y = 20
         ry += 1
         Particle.particles = []
         Particle.part_trail = []
-    #ladda rum
-    load_room(rx, ry)
+        Enemy.enemies = []
+        load_room(rx, ry)
+
 
     #första check för att man står stilla på marken,
     #för att ens momentum inte ska sparas tills när man går av en kant igen
-    checkground1 = player.player_pos_y()
+
 
     #lämna spel
     for event in pygame.event.get():
@@ -342,6 +407,17 @@ while running:
     if player.move_single_axis(0, player_mom_vert):
         player_mom_vert = 3
 
+    for enem in Enemy.enemies:
+        enem.move(0, enem.mom_v)
+        enem.mom_v *= 0.98
+        enem.mom_v += 0.7
+        if enem.move_single_axis(0, enem.mom_v):
+            enem.mom_v = 3
+        enem.track(player.player_pos_x(), player.player_pos_y())
+
+
+
+
 
     #timer för att skjuta
     shot_timer -= 1
@@ -360,7 +436,7 @@ while running:
     if keys[pygame.K_f]:
         if weapon_mode == "normal":
             if shot_timer < 0:
-                Particle.Particle(10, player.player_pos_x() + 8, player.player_pos_y() + 16, (255, 255, 255), mouse_ang, "bullet", 50)
+                Particle.Particle(player.player_pos_x() + 8, player.player_pos_y() + 16, mouse_ang, "bullet", 50)
                 shot_timer = 120
 
     if keys[pygame.K_a]:
@@ -382,14 +458,24 @@ while running:
     for det_b in Detail_back.det_backs:
         pygame.draw.rect(screen, (150, 150, 150), det_b.rect)
 
-    #spelar-sprajtend
+    #spelar-sprajten
     pygame.draw.rect(screen, (255, 255, 255), player.rect)
 
+    #fiender
+    for enem in Enemy.enemies:
+        pygame.draw.rect(screen, enem.color, enem.rect)
+
     for part in Particle.particles:
-        Particle.Particle.move(part, 30, Particle.Particle.reval(part, "ang"))
+        Particle.Particle.move(part, 10, Particle.Particle.reval(part, "ang"), True)
+        for enem in Enemy.enemies:
+            enem.move(0, 0)
+        Particle.Particle.move(part, 10, Particle.Particle.reval(part, "ang"), False)
+        for enem in Enemy.enemies:
+            enem.move(0, 0)
+        Particle.Particle.move(part, 10, Particle.Particle.reval(part, "ang"), False)
         pygame.draw.circle(screen, (255, 255, 255), (part.rect.x, part.rect.y), 4)
         if Particle.Particle.reval(part, "type") == "bullet":
-            Particle.Particle(0, part.rect.x, part.rect.y, (255, 255, 255), mouse_ang, "trail", 500)
+            Particle.Particle(part.rect.x, part.rect.y, mouse_ang, "trail", 500)
             for n in range(5):
                 end_pos_bul_1 = pygame.Vector2(part.rect.x + rand.randint(-5, 5) * 2, part.rect.y + rand.randint(-5, 5) * 2)
                 end_pos_bul_2 = pygame.Vector2(end_pos_bul_1.x + rand.randint(-5, 5) * 2, end_pos_bul_1.y + rand.randint(-5, 5) * 2)
@@ -416,7 +502,7 @@ while running:
                 color_b = 105
             pygame.draw.line(screen, (color_r, color_g, color_b), saved_pos_1, (part.rect.x, part.rect.y), (int(5 * (trail_var / 100)) - 6))
         saved_pos_1 = pygame.Vector2(part.rect.x, part.rect.y)
-        Particle.Particle.move(part, 0, 0)
+        Particle.Particle.move(part, 0, 0, True)
 
     #vapen samt rotation av vapnet
     pygame.draw.polygon(screen, (120, 120, 120), ((player.player_pos_x() + 8 + 12 * math.sin(mouse_ang), player.player_pos_y() + 16 + 12 * math.cos(mouse_ang - math.pi / 12)),
@@ -439,19 +525,6 @@ while running:
     if shot_timer >= 0:
         rect = pygame.Rect(50, 50, shot_timer, 20)
     cd_rect = pygame.draw.rect(screen, (255, 255, 255), rect)
-
-    #andra check för om man står stilla på marken, går i effekt nästa frame
-    checkground2 = player.player_pos_y()
-
-    if (checkground1 - checkground2) == 0:
-        if grounded1:
-            grounded = True
-        else:
-            grounded1 = True
-
-    else:
-        grounded = False
-        grounded1 = False
 
     pygame.display.flip()
 
